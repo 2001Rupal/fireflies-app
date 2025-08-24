d40f101d1b7c9a35f17d794e567afb389e46ce5f
2001Rupal <rupal24porwal@gmail.com>
2025-08-24 14:07:48 +0530

    Updated Code with full UI inhancment

--- FirefliesBackend/Models/FileResult.cs
+++
@@ -1,9 +0,0 @@
-namespace FirefliesBackend.Models
-{
-    public class FileResult
-    {
-        public int Id { get; set; } // DB Primary Key
-        public string Name { get; set; }
-        public string Content { get; set; }
-    }
-}
